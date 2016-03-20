94bfb0a2c3de4a21f5b3c6886fdc3a82c955033d
s2b130050131066 <hinkapatel2994@gmail.com>
2016-03-20 10:56:56 +0530

    Practical 4.20

---
+++ Prac4/4.20/ServletFetchingData.java
@@ -0,0 +1,88 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ServletIFetchingData")
+public class ServletFetchingData extends HttpServlet
+{
+    Connection con=null;
+    Statement stmt=null;
+    ResultSet rs=null;
+    String url="jdbc:mysql://localhost:8080/jdbc";
+    String un="root";
+    String password="hinka";
+    String tn="",tp="";
+    public void init(ServletConfig sc)throws ServletException
+    {
+        super.init(sc);
+        try
+        {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("driver is loaded");
+        con=DriverManager.getConnection(url,un,password);
+        System.out.println("connected");
+        stmt=con.createStatement();
+        System.out.println("wrapper created");
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+            System.out.println("driver Not Loaded"+cnfe.getMessage());
+            return;
+        }
+        catch(SQLException sqle)
+        {
+            System.out.println("connection problem"+sqle.getMessage());
+            return;
+        }
+    }
+    public void doGet(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException
+    {
+        res.setContentType("text/html");
+        PrintWriter out=res.getWriter();
+        doPost(req,res);
+    }
+     public void doPost(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException
+    {
+        res.setContentType("text/html");
+        PrintWriter out=res.getWriter();
+       out.println("<html>");
+       out.println("<body><table>");
+        out.println("<th>name</th>");
+       out.println("<th>password</th>");
+        
+        try
+        {
+            rs=stmt.executeQuery("select *from emp1");
+            while(rs.next())
+            {
+               tn=rs.getString("name");
+               tp=rs.getString("password");
+              
+                out.println("<tr>");
+                out.println("<td>"+tn+"</td>");
+                out.println("<td>"+tp+"</td>");
+                out.println("</tr>");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+       
+       out.println("</table></body></html>");
+    }
+}
